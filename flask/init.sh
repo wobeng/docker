13b8f11c822423731619b3b9b2ccd26229bb6ec3
Welby O <wobeng@yblew.com>
2021-10-09 19:42:19 +0000

    test

--- flask/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ -z "${AWS_EXECUTION_ENV}" ]]; then
-  gunicorn --worker-tmp-dir /dev/shm  --workers $(( 2 * `cat /proc/cpuinfo | grep 'core id' | wc -l` )) --bind 0.0.0.0:$PORT main:app
-else
-  source /lambda-entrypoint.sh "$@"
-fi
