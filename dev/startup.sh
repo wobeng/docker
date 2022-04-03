e4b731d23aa08b58d44437c94eaee4423b7a461f
Welby O <wobeng@yblew.com>
2022-04-03 17:57:18 +0000

    update docker

--- dev/startup.sh
+++ dev/startup.sh
@@ -1,7 +1,7 @@
 # Run workspace-one-time-setup if configured and terminal is interactive
 if [ -t 1 ] && [[ "${TERM_PROGRAM}" = "vscode" || "${TERM_PROGRAM}" = "codespaces" ]] && [ ! -f "$HOME/.config/vscode-dev-containers/workspace-one-time-startup-success" ]; then
-    if [ -f "/usr/local/bin/workspace-one-time-setup.sh" ]; then
-        bash "/usr/local/bin/workspace-one-time-setup.sh"
+    if [ -f "/workspaces/setup.sh" ]; then
+        bash "/workspaces/setup.sh"
     fi
     mkdir -p "$HOME/.config/vscode-dev-containers"
     touch "$HOME/.config/vscode-dev-containers/workspace-one-time-startup-success"
