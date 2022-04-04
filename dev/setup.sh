4465042fbcc5f4ea610fffa543525d4e52173f29
Welby O <wobeng@yblew.com>
2022-04-04 11:54:03 +0000

    update

--- dev/setup.sh
+++ dev/setup.sh
@@ -29,56 +29,6 @@ fi
 script_folder=`pwd`
 workspaces_folder="$(cd "${script_folder}/.." && pwd)"
 
-add-aws-config()
-{
-  rm  -rf ~/.aws/config
-  count=`jq '.aws_roles | length' main.code-workspace`
-  mkdir -p ~/.aws
-  touch ~/.aws/config
-  for ((i=0; i<$count; i++)); do
-      profile=`jq -r '.aws_roles['$i'].profile // empty' main.code-workspace`
-      output=`jq -r '.aws_roles['$i'].output // empty' main.code-workspace`
-      region=`jq -r '.aws_roles['$i'].region // empty' main.code-workspace`
-      role_name=`jq -r '.aws_roles['$i'].role_name // empty' main.code-workspace`
-      role_account_id=`jq -r '.aws_roles['$i'].role_account_id // empty' main.code-workspace`
-      source_profile=`jq -r '.aws_roles['$i'].source_profile // empty' main.code-workspace`
-      sso_start_name=`jq -r '.aws_roles['$i'].sso_start_name // empty' main.code-workspace`
-      sso_role_name=`jq -r '.aws_roles['$i'].sso_role_name // empty' main.code-workspace`
-      sso_account_id=`jq -r '.aws_roles['$i'].sso_account_id // empty' main.code-workspace`
-      {
-          echo "[profile $profile]"
-      } >> ~/.aws/config
-
-      if [ ! -z "$role_name" ]; then
-        {
-          echo "role_arn=arn:aws:iam::$role_account_id:role/$role_name"
-        } >> ~/.aws/config
-      fi
-      if [ ! -z "$source_profile" ]; then
-        {
-          echo "source_profile=$source_profile"
-        } >> ~/.aws/config
-      fi
-      if [ ! -z "$sso_role_name" ]; then
-        {
-          echo "sso_start_url = https://$sso_start_name.awsapps.com/start"
-          echo "sso_region=$region"
-          echo "sso_account_id=$sso_account_id"
-          echo "sso_role_name=$sso_role_name"
-          trigger_sso_signin=$profile
-        } >> ~/.aws/config
-      fi
-      {
-          echo "output=$output"
-          echo "region=$region"
-          echo ""
-      } >> ~/.aws/config
-      if [ ! -z "$trigger_sso_signin" ]; then
-        aws sso login --profile $trigger_sso_signin
-      fi
-  done
-}
-
 clone-repo()
 {
     directory="$(basename "$1")"
@@ -110,12 +60,11 @@ install-requires()
 
 }
 
-
-
   # create envs dir
 mkdir -p /workspaces/envs
 touch /workspaces/envs/dev
-mkdir -p /var/log/exported
+sudo mkdir -p /var/log/exported
+sudo chmod 777 /var/log/exported
 
 echo "ENVIRONMENT=develop" >> /workspaces/envs/dev
 echo "IS_LOCAL=true" >> /workspaces/envs/dev
@@ -175,13 +124,11 @@ if [ -f "${script_folder}/main.code-workspace" ]; then
    else
       echo "Already installed editable pip requirements"
    fi
-   cd "${script_folder}"
 
-  # login aws
-  aws_roles="$(jq -cr '.aws_roles // empty' main.code-workspace)"
-  if [ ! -z "$aws_roles" ]; then
-      add-aws-config
-  fi
+ sudo cp /usr/local/bin/setup.sh ${workspaces_folder}/setup.sh
+ sudo cp /usr/local/bin/sso.sh ${workspaces_folder}/sso.sh
+ sudo chmod +x ${workspaces_folder}/setup.sh
+ sudo chmod +x ${workspaces_folder}/sso.sh
 
 else
   install-requires ${script_folder}
--- dev/startup.sh
+++ dev/startup.sh
@@ -1,7 +1,10 @@
 # Run workspace-one-time-setup if configured and terminal is interactive
 if [ -t 1 ] && [[ "${TERM_PROGRAM}" = "vscode" || "${TERM_PROGRAM}" = "codespaces" ]] && [ ! -f "$HOME/.config/vscode-dev-containers/workspace-one-time-startup-success" ]; then
-    if [ -f "/usr/local/bin/workspace-one-time-setup.sh" ]; then
-        bash "/usr/local/bin/workspace-one-time-setup.sh"
+    if [ -f "/usr/local/bin/setup.sh" ]; then
+        bash "/usr/local/bin/setup.sh"
+    fi
+    if [ -f "/usr/local/bin/sso.sh" ]; then
+        bash "/usr/local/bin/sso.sh"
     fi
     mkdir -p "$HOME/.config/vscode-dev-containers"
     touch "$HOME/.config/vscode-dev-containers/workspace-one-time-startup-success"
