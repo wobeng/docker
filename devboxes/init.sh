415612db457d4f579048508f4c1c84c0251db0e4
Welby O <wobeng@yblew.com>
2022-04-23 11:09:07 +0000

    adding devbox scripts

---
+++ devboxes/init.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -e
+
+# update the system and install packages
+sudo yum update -y && sudo yum install -y rsync jq amazon-efs-utils git zip unzip sssd sssd-tools sssd-ldap openldap-clients rsync
+
+# set up python
+python3  --version
+python3 -m pip install botocore
+
+# start ssm
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+
+# mount efs
+if ! grep -qxF "$EFS_ID:/ /efs efs _netdev,noresvport,tls,iam 0 0" /etc/fstab
+then
+    sudo mkdir -p /efs/home
+    mount -t efs -o tls,iam "$EFS_ID":/ /efs
+    echo "$EFS_ID:/ /efs efs _netdev,noresvport,tls,iam 0 0" >> /etc/fstab
+fi
+
+
+# docker
+sudo amazon-linux-extras install docker -y
+sudo systemctl enable docker
+sudo systemctl restart docker
+sudo usermod -aG docker ec2-user
+
+# add user script
+sudo mkdir -p /etc/pam_scripts
+cat /etc/pam_scripts/login-logger.sh | sed -e "s/EFS_ID/$EFS_ID/" >> /etc/pam_scripts/login-logger.sh
+
+sudo chmod 755 /etc/pam_scripts
+sudo chown root:root -R /etc/pam_scripts
+sudo chmod ugo+x /etc/pam_scripts/login-logger.sh
+
+sudo grep -qxF "session optional pam_exec.so seteuid /etc/pam_scripts/login-logger.sh" /etc/pam.d/sshd || echo "session optional pam_exec.so seteuid /etc/pam_scripts/login-logger.sh" >> /etc/pam.d/sshd
+
+
+# join to domain
+sudo install -d --mode=700 --owner=sssd --group=root /etc/sssd/ldap
+GOOGLE_LDAP_CRT=$(aws ssm get-parameter --name GOOGLE_LDAP_CRT --with-decryption --region us-east-1 | jq -r .Parameter.Value)
+GOOGLE_LDAP_KEY=$(aws ssm get-parameter --name GOOGLE_LDAP_KEY --with-decryption --region us-east-1 | jq -r .Parameter.Value) 
+echo -e "${GOOGLE_LDAP_CRT}" >> /etc/sssd/ldap/google.crt
+echo -e "${GOOGLE_LDAP_KEY}" >> /etc/sssd/ldap/google.key
+sudo chmod 600 /etc/sssd/ldap/google.crt
+sudo chmod 600 /etc/sssd/ldap/google.key
+
+cat /etc/sssd/sssd.conf | sed -e "s/SSSD_DOMAINS/$SSSD_DOMAINS/" >> /etc/sssd/sssd.conf
+cat /etc/sssd/sssd.conf | sed -e "s/SSSD_DOMAIN1/$SSSD_DOMAIN1/" >> /etc/sssd/sssd.conf
+cat /etc/sssd/sssd.conf | sed -e "s/SSSD_SEARCH_BASE1/$SSSD_SEARCH_BASE1/" >> /etc/sssd/sssd.conf
+cat /etc/sssd/sssd.conf | sed -e "s/SSSD_DOMAIN2/$SSSD_DOMAIN2/" >> /etc/sssd/sssd.conf
+cat /etc/sssd/sssd.conf | sed -e "s/SSSD_SEARCH_BASE2/$SSSD_SEARCH_BASE2/" >> /etc/sssd/sssd.conf
+
+sudo authconfig --enablesssd --enablesssdauth --enablemkhomedir --updateall
+
+sudo grep -qxF "sudoers:    files sss" /etc/nsswitch.conf || echo "sudoers:    files sss" >> /etc/nsswitch.conf
+
+sudo chmod 600 /etc/sssd/sssd.conf
+sudo chown 0:0 /etc/sssd/sssd.conf /etc/sssd/ldap/*
+sudo chmod 600 /etc/sssd/sssd.conf /etc/sssd/ldap/*
+sudo restorecon -FRv /etc/sssd
+
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+sudo systemctl enable sssd
+sudo systemctl restart sssd
+sudo systemctl restart sshd
---
+++ devboxes/login.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+username=$(/usr/bin/echo $PAM_USER | cut -d@ -f1)
+domain=$(/usr/bin/echo $PAM_USER | cut -d. -f1 | cut -d@ -f2)
+fulldomain=$(/usr/bin/echo  $PAM_USER | cut -d@ -f2)
+homedir=$(/usr/bin/getent passwd $username | cut -d: -f6)
+efshomedir="/efs$homedir"
+
+if [ "$username" == "root" ] || [ "$username" == "ec2-user" ]; then
+    /usr/bin/echo "Nothing to do for $username"
+    exit 0
+fi
+
+# add user to docker group
+/usr/sbin/usermod -aG docker $username
+
+# mount user efs
+if ! /usr/bin/grep -qxF "EFS_ID:$homedir $homedir efs _netdev,noresvport,tls,iam 0 0" /etc/fstab
+then
+    mkdir -p "$efshomedir"
+    /usr/bin/rsync -a $homedir/ $efshomedir
+
+    chown "$PAM_USER":"$PAM_USER" -R "$efshomedir"
+    /usr/bin/mount -t efs -o tls,iam EFS_ID:"$homedir" "$homedir"
+    echo "EFS_ID:$homedir $homedir efs _netdev,noresvport,tls,iam 0 0" >> /etc/fstab
+fi
+
+# make sure there is a ssh key
+if [ ! -f "$homedir/.ssh/ed25519_$domain" ]; then
+    mkdir -p "$homedir/.ssh"
+    touch "$homedir/.ssh/authorized_keys"
+    /usr/bin/ssh-keygen -q -t ed25519 -N '' -f "$homedir/.ssh/ed25519_$domain" <<<y >/dev/null 2>&1
+    /usr/bin/cat "$homedir/.ssh/ed25519_$domain.pub" > "$homedir/.ssh/authorized_keys"
+    chmod 700 "$homedir/.ssh"
+    chmod 600 "$homedir/.ssh/authorized_keys"
+    chown "$PAM_USER":"$PAM_USER" -R "$homedir/.ssh"
+fi
