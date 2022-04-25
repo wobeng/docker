9b1c67fdab6ddb4d8531c19b4b50d2b324ae4302
Welby O <wobeng@yblew.com>
2022-04-25 09:27:58 +0000

    ensure agent is running

--- devboxes/login.sh
+++ devboxes/login.sh
@@ -19,6 +19,16 @@ then
     mkdir -p "$efshomedir"
     mkdir -p "/tmp/$userid"
     chown "$userid":"$userid" -R "/tmp/$userid"
+
+    /usr/bin/git config --global user.name "$username"
+    /usr/bin/git config --global user.email "$PAM_USER"
+
+    # add user to docker group
+    /usr/sbin/usermod -aG docker $username
+    /usr/sbin/usermod -aG docker $PAM_USER
+
+
+    # mount
     /usr/bin/rsync -a $homedir/ $efshomedir
     echo "export TMPDIR=/tmp/$userid" >> "$efshomedir/.bashrc"
     echo "export USER_EMAIL=$PAM_USER" >> "$efshomedir/.bashrc"
@@ -41,16 +51,19 @@ if [ ! -f "$homedir/.ssh/ed25519_$domain" ]; then
     } >> "$homedir/.ssh/config"
 fi
 
-# add user to docker group
-/usr/sbin/usermod -aG docker $username
-/usr/sbin/usermod -aG docker $PAM_USER
-
 
 # make sure user can always login
 chmod 700 "$homedir/.ssh"
 chmod 600 "$homedir/.ssh/config"
 chmod 600 "$homedir/.ssh/authorized_keys"
 chown "$userid":"$userid"  -R "$homedir/.ssh"
+
+# ensure ssh agent is running
+ if [ ! -S $homedir/.ssh/ssh_auth_sock ]; then
+   eval `/usr/bin/ssh-agent`
+   /usr/bin/ln -sf "$SSH_AUTH_SOCK" $homedir/.ssh/ssh_auth_sock
+ fi
+ export SSH_AUTH_SOCK=$homedir/.ssh/ssh_auth_sock
 
 # make devboxes
 mkdir -p "$homedir/devbox/.devcontainer"
