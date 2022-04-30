6b6d72982e9c1e2e5eb72b01e50485e57e0b2ee8
Welby O <wobeng@yblew.com>
2022-04-30 17:32:51 +0000

    fix spacing

--- devboxes/bash_scripts/auth_keys.sh
+++ devboxes/bash_scripts/auth_keys.sh
@@ -7,15 +7,15 @@ PAM_USER=${0}
 
 
 homedir=$(/usr/bin/getent passwd "${PAM_USER}" | cut -d: -f6 ) > /dev/null 2>&1
-getent _exit="${?}"
+getent_exit="${?}"
 
-if [ "${getent _exit}" -eq 0 ] ; then
+if [ "${getent_exit}" -eq 0 ] ; then
     # User actually exist.  Continue
     
     efshomedir="/efs$homedir"
     /usr/bin/cat "$efshomedir/.ssh/id_ed25519.pub" 
-    cat _exit="${?}"
-    if [ "${cat _exit}" -eq 0 ] ; then
+    cat_exit="${?}"
+    if [ "${cat_exit}" -eq 0 ] ; then
         # User ssh key exist. exit nicely
         exit 0
     fi
