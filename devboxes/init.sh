7adc2d0ffdb31b184cf14b6422f1c886d8e9b4f0
Welby O <wobeng@yblew.com>
2022-04-30 17:18:15 +0000

    fix docker install

--- devboxes/init.sh
+++ devboxes/init.sh
@@ -54,22 +54,10 @@ then
 fi
 
 # Install docker and mount docker volumes to efs
-if ! grep -qxF "$EFS_ID:/docker/volumes /var/lib/docker/volumes efs _netdev,noresvport,tls,iam 0 0" /etc/fstab
-
-then
-    sudo amazon-linux-extras install docker -y
-
-    sudo mkdir -p /efs/docker/volumes
-    sudo mkdir -p /var/lib/docker/volumes
-    mount -t efs -o tls,iam "$EFS_ID":/docker/volumes /var/lib/docker/volumes
-    echo "$EFS_ID:/docker/volumes /var/lib/docker/volumes efs _netdev,noresvport,tls,iam 0 0" >> /etc/fstab
-
-    sudo systemctl enable docker
-    sudo systemctl restart docker
-    sudo usermod -aG docker ec2-user   
-
-fi 
-
+sudo amazon-linux-extras install docker -y
+sudo systemctl enable docker
+sudo systemctl restart docker
+sudo usermod -aG docker ec2-user   
 
 # add user script
 sudo mkdir -p /etc/pam_scripts
