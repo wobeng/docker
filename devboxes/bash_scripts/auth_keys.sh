268be8a50b8a7fb345ea6623965d1d48e2c208c2
welbyobeng <wobeng@yblew.com>
2022-06-04 10:43:18 +0000

    refactor

--- devboxes/bash_scripts/auth_keys.sh
+++ devboxes/bash_scripts/auth_keys.sh
@@ -9,12 +9,15 @@ statePath="/etc/pam_scripts/users/$domain-state.json"
 stateUsersPath="/etc/pam_scripts/users/$domain-users.json"
 stateUrl="https://s3.amazonaws.com/public-gws-aws.$domain"
 
-
-datetime=$(jq -r .lastSync $statePath)
-
-if [ $? -ne 0 ]; then
+exit_nicely()
+{
     /usr/bin/timeout 5s "/opt/aws/bin/eic_run_authorized_keys" "$@" 
     exit 0
+}
+
+datetime=$(jq -r .lastSync $statePath)
+if [ $? -ne 0 ]; then
+    exit_nicely "$@"
 fi
 
 timeago='15 min ago'
@@ -23,19 +26,19 @@ taSec=$(date --date "$timeago" +'%s')
 
 if [ $dtSec -lt $taSec  ]; then
     echo "sync is too old"
-    exit 1
+    exit_nicely "$@"
 fi
 
 username=$(cat $stateUsersPath | jq -r ".users[] | select(.email==\"$username\") | .email")
 if [ $? -ne 0 ]; then
     echo "username does not exist"
-    exit 1
+    exit_nicely "$@"
 fi
 
 pubkey=$(curl -H 'Cache-Control: no-cache, no-store' --fail-with-body -s $stateUrl/users/keys/${username}.pub)
 if [ $? -ne 0 ]; then
     echo "something went wrong with pub key"
-    exit 1
+    exit_nicely "$@"
 fi
 
 
