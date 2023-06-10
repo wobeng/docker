741fa0913f6f784b90bc46b2f4585e77813b2c6f
Welby O <wobeng@users.noreply.github.com>
2023-06-10 14:14:20 -0400

    Update devbox.sh

--- devboxes-v2/user_scripts/devbox.sh
+++ devboxes-v2/user_scripts/devbox.sh
@@ -7,6 +7,34 @@ target="$USER_WORKSPACE/repos/$folder"
 echo "==================== START DEVBOX ===================="
 echo ""
 echo ""
+
+# setup env
+mkdir -p "$USER_WORKSPACE/repos"
+mkdir -p "$USER_WORKSPACE/extras"
+mkdir -p "$USER_WORKSPACE/configs/envs"
+mkdir -p "$USER_WORKSPACE/configs/installs"
+ln -s "$HOME/.aws" $USER_WORKSPACE/configs/aws
+ln -s "$HOME/.gcloud" $USER_WORKSPACE/configs/gcloud
+touch $USER_WORKSPACE/configs/envs/dev
+
+if ! grep -qxF "ENVIRONMENT=develop" $USER_WORKSPACE/configs/envs/dev
+then
+    echo "ENVIRONMENT=develop" >> $USER_WORKSPACE/configs/envs/dev
+fi
+if ! grep -qxF "IS_LOCAL=true" $USER_WORKSPACE/configs/envs/dev
+then
+    echo "IS_LOCAL=true" >> $USER_WORKSPACE/configs/envs/dev
+fi
+
+if [ ! -d "$USER_WORKSPACE/extras/virenv" ]; then
+    python3 -m venv $USER_WORKSPACE/extras/virenv
+    $USER_WORKSPACE/extras/virenv/bin/python -m pip install --upgrade pip
+    $USER_WORKSPACE/extras/virenv/bin/python -m pip install --upgrade ruff
+    $USER_WORKSPACE/extras/virenv/bin/python -m pip install --upgrade pytest
+    $USER_WORKSPACE/extras/virenv/bin/python -m pip install --upgrade pre-commit
+
+fi
+
 man () {
      echo "Oops, Something went wrong"
      echo "Command format: devbox setup <org>/<username>"
