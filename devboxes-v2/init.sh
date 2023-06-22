df5a5f6cffb9a9ce325f38a2444c9e7a825c2ca2
Welby O <wobeng@users.noreply.github.com>
2023-06-21 20:57:33 -0400

    Update init.sh

--- devboxes-v2/init.sh
+++ devboxes-v2/init.sh
@@ -116,20 +116,6 @@ sudo /usr/sbin/sysctl -p
 # install ssl
 # sudo certbot certonly -i nginx --dns-route53 --no-redirect -d "${INSTANCE_NAME}.${INSTANCE_DOMAIN}" -d "*.${INSTANCE_NAME}.${INSTANCE_DOMAIN}" --non-interactive --agree-tos --register-unsafely-without-email --expand
 
-# change over iam role from devboxes-admin to devboxes
-sudo mkdir ~/.aws && chmod 700 ~/.aws
-sudo touch ~/.aws/credentials
-/usr/local/bin/aws iam create-user --user-name $INSTANCE_ID
-/usr/local/bin/aws iam add-user-to-group --user-name $INSTANCE_ID --group-name devboxes-admin
-keys=$(/usr/local/bin/aws iam create-access-key --user-name $INSTANCE_ID)
-aid=$(/usr/local/bin/aws ec2 describe-iam-instance-profile-associations  --region us-east-1 --filters Name=instance-id,Values=$INSTANCE_ID | jq --raw-output  .IamInstanceProfileAssociations[0].AssociationId)
-/usr/local/bin/aws ec2 replace-iam-instance-profile-association --iam-instance-profile Name=devboxes --association-id $aid
-echo "[default]" >> ~/.aws/credentials
-echo "aws_access_key_id=$(echo $keys | jq --raw-output .AccessKey.AccessKeyId)" >> ~/.aws/credentials
-echo "aws_secret_access_key=$(echo $keys | jq --raw-output .AccessKey.SecretAccessKey)" >> ~/.aws/credentials
-chmod 600  ~/.aws/credentials
-
-
 # create users
 sudo /bin/bash -c '/etc/pam_scripts/users.sh' >> /var/log/create-users.log
 
@@ -153,6 +139,19 @@ touch /var/spool/cron/root
 /usr/bin/crontab /var/spool/cron/root
 echo "*/5 * * * * cd /root && /bin/bash -c '/etc/pam_scripts/users.sh' >> /var/log/create-users.log 2>&1" >> /var/spool/cron/root
 echo "0 * * * * cd /root && sudo certbot renew -i nginx --dns-route53 --no-redirect --non-interactive --agree-tos --register-unsafely-without-email --expand" >> /var/spool/cron/root
+
+# change over iam role from devboxes-admin to devboxes
+sudo mkdir ~/.aws && chmod 700 ~/.aws
+sudo touch ~/.aws/credentials
+/usr/local/bin/aws iam create-user --user-name $INSTANCE_ID
+/usr/local/bin/aws iam add-user-to-group --user-name $INSTANCE_ID --group-name devboxes-admin
+keys=$(/usr/local/bin/aws iam create-access-key --user-name $INSTANCE_ID)
+aid=$(/usr/local/bin/aws ec2 describe-iam-instance-profile-associations  --region us-east-1 --filters Name=instance-id,Values=$INSTANCE_ID | jq --raw-output  .IamInstanceProfileAssociations[0].AssociationId)
+/usr/local/bin/aws ec2 replace-iam-instance-profile-association --iam-instance-profile Name=devboxes --association-id $aid
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id=$(echo $keys | jq --raw-output .AccessKey.AccessKeyId)" >> ~/.aws/credentials
+echo "aws_secret_access_key=$(echo $keys | jq --raw-output .AccessKey.SecretAccessKey)" >> ~/.aws/credentials
+chmod 600  ~/.aws/credentials
 
 sudo sed -i 's/#Port 22/Port 55977/g' /etc/ssh/sshd_config
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
